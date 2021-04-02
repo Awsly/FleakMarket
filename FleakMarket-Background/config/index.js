39cfcb4e76597731c37a458342ed6144e56ded63
Awsly <49740607+Awsly@users.noreply.github.com>
2021-04-02 19:26:21 +0800

    Update README.md
    联系方式

--- FleakMarket-Background/config/index.js
+++ FleakMarket-Background/config/index.js
@@ -10,25 +10,17 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      '/api': {
-          target: 'http://192.168.22.1:9999',
-          changeOrigin: true,
-          pathRewrite: {
-              '^/api' : ''
-          }
-      }
-    },
+    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-
+    
     /**
      * Source Maps
      */
